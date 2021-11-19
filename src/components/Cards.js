0203fd970821bda23716df5545fa195f7de49d51
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-11-18 20:08:51 -0500

    card update and file structure

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,6 +4,17 @@ function Cards() {
     return (
         <div className="cards">
             <h1>Check out some of my Projects!</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <div className="cards__items">
+                        <CardItem 
+                        src="images/img-9.jpg"
+                        text="Project 9"
+                        label="Code Quiz"
+                        path="/projects"/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
         
