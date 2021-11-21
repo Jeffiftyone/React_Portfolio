dbea46ac15ba6d7cf23093d31c034308274df13b
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2021-11-21 03:35:55 -0500

    final deploy

---
+++ build/precache-manifest.4b0a3605f3711cd0c8b9d2c87d36c09f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b37aaf740991ddfaaaff95944125eeda",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "060d13a05be6cacd0daa",
+    "url": "/React_Portfolio/static/css/main.cfe0546f.chunk.css"
+  },
+  {
+    "revision": "95ed024f4125d61b3b98",
+    "url": "/React_Portfolio/static/js/2.e83220cb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React_Portfolio/static/js/2.e83220cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060d13a05be6cacd0daa",
+    "url": "/React_Portfolio/static/js/main.1b702994.chunk.js"
+  },
+  {
+    "revision": "76fa0f473dd6d2bf0a74",
+    "url": "/React_Portfolio/static/js/runtime-main.cbad1128.js"
+  }
+]);
