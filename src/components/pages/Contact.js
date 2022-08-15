4870cd863e694eb942aa716e0f1935ae33d600f2
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2022-08-14 20:26:52 -0400

    contact update

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -5,12 +5,16 @@ import React from 'react';
 function Contact() {
 
   return (
-    <div className="contact">
-      <p>
-    To contact me please Email me at jeffiftyone@gmail.com <br/> 
-    or connect with me on <a href="https://www.linkedin.com/in/jeffreybeh/">LinkedIn</a>
-    </p>
+    <div className="contact h-full flex justify-center flex-col">
+      <div className="w-2/3 m-auto p-8 shadow-lg">
+        <h1>
+        <i class="fas fa-comment-alt"></i> &nbsp;
+        <span>Contact Me</span>
+        </h1>
+      </div>
+
     </div>
+    
   );
 }
 
