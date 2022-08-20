42ad1894c120f2001d32eebb3ada976fb372a48b
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2022-08-19 23:04:51 -0400

    contact update

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,24 +1,54 @@
 import '../../App.css';
 
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
-function Contact() {
+export const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mk32l2p', 'template_jr43fg', form.current, '2k-JxL_9QErYMB-WjG8my')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
     <>
-   <container className="contact"> 
-    <div class="col-sm"> 
-    <div class="container">
-      <div className="card">
-      <header><h2><i class="fas fa-paper-plane"></i> &nbsp;Contact Me</h2>
+    <container className="contact"> 
+     <div class="col-sm"> 
+     <div class="container">
+       <div className="card">
+       <header><h2><i class="fas fa-paper-plane"></i> &nbsp;Contact Me</h2>
       </header>
-      <p>Form here</p>
+   <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <br/>
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <br/>
+      <label>Message</label>
+      <textarea name="message" />
+      <br/>
+      
+      <button type="submit" class="btn btn-dark btn-block" >
+          <span>Send &nbsp;<i class="fas fa-paper-plane"></i></span>
+      </button>
+    </form>
+
     </div>
    </div>
   </div>
   </container>
   </>
   );
-}
+};
+
+
 
 export default Contact;
