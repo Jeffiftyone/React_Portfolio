930ed316b12b62bd178c52d4e1a997a5b5d613d1
Jeffrey.Beh <jeffrey.beh@hotmail.com>
2022-09-17 15:01:25 -0400

    working contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,12 +2,28 @@ import '../../App.css';
 
 import React, { useRef } from 'react';
 import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Contact = () => {
+  
+const notify = () =>{
+  toast.success('Message Sent!', {
+    position: "top-center",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });;
+};
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
+    notify();
+    e.target.reset();
 
     emailjs.sendForm('service_mk32l2p', 'template_jrc43fg', form.current, 'AZx7PyMfx0jFCiJwd')
       .then((result) => {
@@ -17,34 +33,50 @@ export const Contact = () => {
       });
   };
 
+
   return (
     <>
     <container className="contact"> 
      <div class="col-sm"> 
      <div class="container">
        <div className="contact-card">
-       <header><h2><i class="fas fa-paper-plane"></i> &nbsp;Contact Me</h2>
+       <header><h2><i class="fas fa-paper-plane"></i> &nbsp;Write me a Message!</h2>
       </header>
    <form ref={form} onSubmit={sendEmail}>
+      
+      <div class="input-group">
       <label>Name</label>
-      <input type="text" name="user_name" />
-      <br/>
+      <input type="text" name="user_name" required />
+      </div>
+      <div class="input-group">
       <label>Email</label>
-      <input type="email" name="user_email" />
-      <br/>
+      <input type="email" name="user_email" required />
+      </div>
+      <div class="input-group">
       <label>Message</label>
-      <textarea name="message" />
-      <br/>
+      <textarea name="message" required />
+      </div>
+      
       
       <button type="submit" class="btn btn-dark btn-block" >
           <span>Send &nbsp;<i class="fas fa-paper-plane"></i></span>
       </button>
     </form>
-
     </div>
    </div>
   </div>
   </container>
+  <ToastContainer
+    position="top-center"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    />
   </>
   );
 };
